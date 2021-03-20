2626018d50b0a00f9e25e04bc3be6b13d80b3d0c
JoShinyeong <Shonyeong06@hanmail.net>
2021-03-20 16:13:21 +0900

    Table_Reservation

--- eatgo-customer-api/src/test/java/kr/co/fastcampus/eatgo/application/UserServiceTests.java
+++ eatgo-customer-api/src/test/java/kr/co/fastcampus/eatgo/application/UserServiceTests.java
@@ -23,98 +23,47 @@ import static org.mockito.Mockito.verify;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class UserServiceTests {
 
+
     @InjectMocks
     private UserService userService;
 
     @Mock
-    private UserRepository userRepository;
+    private UserRepository userRepostory;
 
     @Mock
     private PasswordEncoder passwordEncoder;
 
     @Before
-    public void  setUp(){
-
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
-
-        userService = new UserService(userRepository,passwordEncoder);
     }
 
     @Test
-    public  void registerUser(){
+    public void registerUser() {
         String email = "tester@example.com";
         String name = "Tester";
         String password = "test";
 
         userService.registerUser(email, name, password);
 
-        verify(userRepository).save(any());
+        verify(userRepostory).save(any());
     }
 
-
     @Test
-    public void registerUserWithExistedEmail(){
+    public void registerUserWithExistedEmail() {
         String email = "tester@example.com";
         String name = "Tester";
         String password = "test";
 
         User user = User.builder().build();
+        given(userRepostory.findByEmail(email)).willReturn(Optional.of(user));
 
-        given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
-
-        assertThatThrownBy(()->{
+        assertThatThrownBy(() -> {
             userService.registerUser(email, name, password);
         }).isInstanceOf(EmailExistedException.class);
 
-        verify(userRepository, never()).save(any());
+        verify(userRepostory, never()).save(any());
     }
 
 
-    @Test
-    public void authenticateWithValidAttributes(){
-        String email = "tester@example.com";
-        String password = "test";
-
-
-        User mockUser = User.builder().email(email).build();
-
-        given(userRepository.findByEmail(email)).willReturn(Optional.of(mockUser));
-
-        given(passwordEncoder.matches(any(), any())).willReturn(true);
-
-        User user = userService.authenticate(email, password);
-
-        assertThat(user.getEmail(), is(email));
-
-    }
-
-    @Test(expected = EmailNotExistedException.class)
-    public void authenticateWithNotExistedEmail(){
-        String email = "x@example.com";
-        String password = "test";
-
-        given(userRepository.findByEmail(email)).willReturn(Optional.empty());
-
-
-
-
-        userService.authenticate(email, password);
-
-    }
-
-    @Test(expected = PasswordWrongException.class)
-    public void authenticateWithWrongPassword(){
-        String email = "tester@example.com";
-        String password = "x";
-
-        User mockUser = User.builder().email(email).build();
-
-        given(userRepository.findByEmail(email)).willReturn(Optional.of(mockUser));
-
-        given(passwordEncoder.matches(any(), any())).willReturn(false);
-
-
-        userService.authenticate(email, password);
-
-    }
 }
---
+++ eatgo-restaurant-api/src/test/java/kr/co/fastcampus/eatgo/EatgoReservationApiApplicationTests.java
@@ -0,0 +1,15 @@
+package kr.co.fastcampus.eatgo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+
+
+@SpringBootTest
+public class EatgoReservationApiApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+
+}
